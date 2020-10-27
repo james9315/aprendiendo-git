580f1d4479ff80f5eda85721c2fa22c7d0da0947
James Grisales <john.341018352@ucaldas.edu.co>
2020-10-26 21:42:01 -0500

    se hacen cambios en title y imagen

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'proyecto git',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
           body: Container(
+            padding: EdgeInsets.only(
+              top: 150,
+              bottom: 10,
+              right: 10,
+              left: 10,
+            ),
             decoration: BoxDecoration(
                 image: DecorationImage(
                     image: NetworkImage(
-                        "https://www.google.com/search?q=imagenes+programacion&rlz=1C1CHBF_esCO918CO918&sxsrf=ALeKk00LJ8UwDFmYKap7ns94npXTJR8YDA:1603673585661&tbm=isch&source=iu&ictx=1&fir=BMtmYGmgEVEbWM%252CB1n5Q9NP2JL0xM%252C_&vet=1&usg=AI4_-kS05npEVu2Un80kxmUfB3RD63phwg&sa=X&ved=2ahUKEwi6s6LThdHsAhVwrlkKHYBEA58Q9QF6BAgKEFo&biw=1366&bih=657#imgrc=BMtmYGmgEVEbWM"))),
+                        "https://www.google.com/search?q=imagenes+programacion&rlz=1C1CHBF_esCO918CO918&sxsrf=ALeKk01zhenSWSChh2f4vDrfnUlUGjDcdg:1603761136835&tbm=isch&source=iu&ictx=1&fir=r1GQTniOfHo66M%252CfXXdZvFcXxAPXM%252C_&vet=1&usg=AI4_-kTzNZwrYWVB15uN4uBHTZN7Mr3OjQ&sa=X&ved=2ahUKEwi___Xmy9PsAhULjVkKHcapA2MQ9QF6BAgKEFA&biw=1366&bih=657#imgrc=r1GQTniOfHo66M"),
+                    alignment: Alignment.topCenter)),
             child: Column(
               children: <Widget>[
                 Row(
@@ -58,8 +65,149 @@ class MyApp extends StatelessWidget {
                               width: 50,
                               height: 50,
                               child: Center(
-                                child: Text("Funciones",
+                                child: Text("Variables",
                                     textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RaisedButton(
+                            color: Colors.blueGrey,
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {},
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child: Text("condiciones",
+                                    textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RaisedButton(
+                            color: Colors.blueGrey,
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {},
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child: Text("Flutter",
+                                    textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RaisedButton(
+                            color: Colors.blueGrey,
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {},
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child:
+                                    Text("dart", textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RaisedButton(
+                            color: Colors.blueGrey,
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {},
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child:
+                                    Text("Widget", textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RaisedButton(
+                            color: Colors.blueGrey,
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {},
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child: Text("Programacion OPP",
+                                    textAlign: TextAlign.center),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: RaisedButton(
+                            color: Colors.blueGrey,
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            onPressed: () {},
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Center(
+                                child:
+                                    Text("Ciclos", textAlign: TextAlign.center),
                               ),
                             ),
                           ),
